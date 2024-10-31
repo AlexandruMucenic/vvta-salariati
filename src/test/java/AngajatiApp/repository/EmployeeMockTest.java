3a988c2187e644ce5a3f00a947b3a9b05d10a473
alexandrumucenic <alexmucenic6@gmail.com>
2024-10-31 13:01:31 +0200

    lab 3

--- src/test/java/AngajatiApp/repository/EmployeeMockTest.java
+++ src/test/java/AngajatiApp/repository/EmployeeMockTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeMockTest {
@@ -43,5 +46,30 @@ class EmployeeMockTest {
     void addEmployee_TC04() {
         Employee employee = new Employee("Mucenic", "Alex", "1234567890123", DidacticFunction.ASSISTENT, 0.0);
         assertFalse(employeeMock.addEmployee(employee));
+    }
+
+    //    TESTE MODIFY EMPLOYEE
+    @Test
+    void modifyEmployee_TC01() {
+        Employee mihai = employeeMock.getEmployeeList().stream()
+                .filter(emp -> "Ion".equals(emp.getFirstName()) && "Dumitrescu".equals(emp.getLastName()))
+                .findFirst()
+                .orElse(null);
+
+        employeeMock.modifyEmployeeFunction(mihai, DidacticFunction.ASSISTENT);
+        assert mihai != null;
+        assertEquals(DidacticFunction.ASSISTENT, mihai.getFunction());
+    }
+
+    @Test
+    void modifyEmployee_TC02() {
+        List<DidacticFunction> originalFunctions = employeeMock.getEmployeeList().stream()
+                .map(Employee::getFunction)
+                .collect(Collectors.toList());
+
+        employeeMock.modifyEmployeeFunction(null, DidacticFunction.ASSISTENT);
+        for (int i = 0; i < employeeMock.getEmployeeList().size(); i++) {
+            assertEquals(originalFunctions.get(i), employeeMock.getEmployeeList().get(i).getFunction());
+        }
     }
 }
